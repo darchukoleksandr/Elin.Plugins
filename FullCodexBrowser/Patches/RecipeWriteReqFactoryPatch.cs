2b983e0951c860512e5d926cf5b36d9890be8358
Darchuk Oleksandr <darchukoleksandr@gmail.com>
2024-12-20 13:16:21 +0200

    Added info to recipe is it learned or not

---
+++ FullCodexBrowser/Patches/RecipeWriteReqFactoryPatch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System;
+
+namespace FullCodexBrowser.Patches
+{
+	[HarmonyPatch(typeof(Recipe))]
+	[HarmonyPatch(nameof(Recipe.WriteReqFactory))]
+	public static class RecipeWriteReqFactoryPatch
+	{
+		public static bool Prefix(Recipe __instance, UINote n)
+		{
+			try
+			{
+				if (__instance.source.NeedFactory)
+				{
+					int num = EMono.player.recipes.knownRecipes.TryGetValue(__instance.id, 0);
+					if (num > 0)
+					{
+						//note.AddHeaderTopic("reqFactory".lang("Recipe learned".TagColor(FontColor.Bad)));
+						n.AddHeaderTopic("Recipe learned".TagColor(FontColor.Good));
+					}
+					else
+					{
+						n.AddHeaderTopic("Recipe not learned".TagColor(FontColor.Bad));
+					}
+				}
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Plugin.Log("Error: " + ex.Message);
+				return true;
+			}
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Elin mods
+
+## Released
+- [Fast travel redone](./FastTravelRedone/)
+- [Full codex browser](./FullCodexBrowser/)
+- [Go arount that tree](./GoArountThatTree/)
+- [Negotiation redone](./NegotiationRedone/)
